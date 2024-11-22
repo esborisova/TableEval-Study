173871bef66ab546ca83d6c6e4d0d442ad7118d6
Ekaterina Borisova <89642052+esborisova@users.noreply.github.com>
2024-11-22 15:49:46 +0100

    black formatted

--- src/application/get_parsed_pmc_tabs.py
+++ src/application/get_parsed_pmc_tabs.py
@@ -2,20 +2,22 @@ from datasets import Dataset, DatasetDict, load_dataset, load_from_disk
 from datetime import datetime
 from ..utils.parse_xml_table import parsed_tab_xml_df
 
+
 def main():
     ds = load_from_disk("../../../data/ComTQA_data/comtqa_updated_2024-11-20")
     comtqa_df = ds["train"].to_pandas()
     comtqa_df[["table_headers", "table_subheaders", "table_rows"]] = comtqa_df.apply(
-        parsed_tab_xml_df, 
-        axis=1, 
-        condition_col="dataset", 
+        parsed_tab_xml_df,
+        axis=1,
+        condition_col="dataset",
         condition_value="PubTab1M",
-        xml_col = "table_xml"
+        xml_col="table_xml",
     )
     dataset = Dataset.from_pandas(comtqa_df)
     dataset_dict = DatasetDict({"train": dataset})
     date = datetime.now().strftime("%Y-%m-%d")
     dataset_dict.save_to_disk(f"../../../data/ComTQA_data/comtqa_updated_{date}")
+
 
 if __name__ == "__main__":
     main()
--- src/utils/parse_xml_table.py
+++ src/utils/parse_xml_table.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 from html import unescape
 from typing import Tuple, List
 
+
 def get_max_columns(root: ET.Element) -> int:
     "Computes the max number of columns across all header rows"
     max_columns = 0
@@ -11,65 +12,74 @@ def get_max_columns(root: ET.Element) -> int:
         header_rows = thead.findall(".//tr")
         for header_row in header_rows:
             header_cells = [
-                unescape("".join(cell.itertext()).strip()) for cell in header_row.findall("td")
+                unescape("".join(cell.itertext()).strip())
+                for cell in header_row.findall("td")
             ]
             max_columns = max(max_columns, len(header_cells))
     return max_columns
+
 
 def extract_headers(root: ET.Element, max_columns: int) -> Tuple[List[str], List[str]]:
     "Extracts headers and subheaders"
     headers = []
     subheaders = []
-    
+
     thead = root.find(".//thead")
     if thead is not None:
         header_rows = thead.findall(".//tr")
-        
+
         for i, header_row in enumerate(header_rows):
             header_cells = [
-                unescape("".join(cell.itertext()).strip()) for cell in header_row.findall("td")
+                unescape("".join(cell.itertext()).strip())
+                for cell in header_row.findall("td")
             ]
-            
-            # Fill in empty cells with empty str 
+
+            # Fill in empty cells with empty str
             while len(header_cells) < max_columns:
                 header_cells.append("")
             if i == 0:
-                headers = header_cells  
+                headers = header_cells
             else:
-                subheaders.append(header_cells)  
+                subheaders.append(header_cells)
     return headers, subheaders
+
+
 def extract_rows(root: ET.Element, max_columns: int) -> List[str]:
     rows = []
-    
+
     tbody = root.find(".//tbody")
     if tbody is not None:
         for row in tbody.findall("tr"):
-            cols = [unescape("".join(col.itertext()).strip()) for col in row.findall("td")]
-            # Fill in empty cells with empty str 
+            cols = [
+                unescape("".join(col.itertext()).strip()) for col in row.findall("td")
+            ]
+            # Fill in empty cells with empty str
             while len(cols) < max_columns:
                 cols.append("")
             rows.append(cols)
     return rows
+
+
 def parse_table(xml_content: str) -> dict:
     "Extracts headers, subheaders, and rows from table xml"
     root = ET.fromstring(xml_content)
-    
-    table_data = {
-        "table_headers": [],
-        "table_subheaders": [],
-        "table_rows": []
-    }
-    
+
+    table_data = {"table_headers": [], "table_subheaders": [], "table_rows": []}
+
     max_columns = get_max_columns(root)
-    table_data["table_headers"], table_data["table_subheaders"] = extract_headers(root, max_columns)
+    table_data["table_headers"], table_data["table_subheaders"] = extract_headers(
+        root, max_columns
+    )
     table_data["table_rows"] = extract_rows(root, max_columns)
-    
+
     return table_data
 
 
-def parsed_tab_xml_df(row: pd.Series, condition_col: str, condition_value: str, xml_col: str) -> pd.Series :
+def parsed_tab_xml_df(
+    row: pd.Series, condition_col: str, condition_value: str, xml_col: str
+) -> pd.Series:
     """
-    Parses table XML and saves into a series. 
+    Parses table XML and saves into a series.
 
     Args:
         row (pd.Series): A single row of the DataFrame being processed.
@@ -82,14 +92,14 @@ def parsed_tab_xml_df(row: pd.Series, condition_col: str, condition_value: str, 
     """
     if row[condition_col] == condition_value:
         table_data = parse_table(row[xml_col])
-        return pd.Series({
-            "table_headers": table_data["table_headers"],
-            "table_subheaders": table_data["table_subheaders"],
-            "table_rows": table_data["table_rows"]
-        })
+        return pd.Series(
+            {
+                "table_headers": table_data["table_headers"],
+                "table_subheaders": table_data["table_subheaders"],
+                "table_rows": table_data["table_rows"],
+            }
+        )
     else:
-        return pd.Series({
-            "table_headers": None,
-            "table_subheaders": None,
-            "table_rows": None
-        })
+        return pd.Series(
+            {"table_headers": None, "table_subheaders": None, "table_rows": None}
+        )

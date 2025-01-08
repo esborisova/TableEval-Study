e1c808792202b4f9c0576743f2c245c4cd5835be
Ekaterina Borisova <89642052+esborisova@users.noreply.github.com>
2025-01-08 20:16:47 +0100

    black format

--- src/application/scrap_source_tex_scigen.py
+++ src/application/scrap_source_tex_scigen.py
@@ -5,25 +5,31 @@ import logging
 import tarfile
 from datasets import load_from_disk
 
-logging.basicConfig(filename='download_errors.log', level=logging.ERROR, 
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename="download_errors.log",
+    level=logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
 
 def create_dir(root_dir: str):
     if not os.path.exists(root_dir):
         os.makedirs(root_dir)
 
+
 def fetch_arxiv_source(arxiv_url: str):
     try:
         response = requests.get(arxiv_url)
-        response.raise_for_status() 
+        response.raise_for_status()
         return response
     except Exception as e:
         logging.error(f"Error downloading from {arxiv_url}: {e}")
         return None
 
+
 def save_to_file(zip_path: str, content):
     try:
-        with open(zip_path, 'wb') as f:
+        with open(zip_path, "wb") as f:
             f.write(content)
         print(f"Downloaded successfully to {zip_path}")
         return zip_path
@@ -31,44 +37,55 @@ def save_to_file(zip_path: str, content):
         logging.error(f"Error saving to {zip_path}: {e}")
         return None
 
+
 def download_arxiv_source(arxiv_id: str, download_path: str):
-    arxiv_url = f'https://arxiv.org/e-print/{arxiv_id}'
+    arxiv_url = f"https://arxiv.org/e-print/{arxiv_id}"
     print(f"Downloading from {arxiv_url}")
     create_dir(download_path)
     response = fetch_arxiv_source(arxiv_url)
     if not response:
         return None
-    zip_path = os.path.join(download_path, f'{arxiv_id}.tar.gz')
+    zip_path = os.path.join(download_path, f"{arxiv_id}.tar.gz")
     return save_to_file(zip_path, response.content)
 
+
 def extract_all_tar_gz_in_dir(directory: str, save_path: str):
-    
+
     for filename in os.listdir(directory):
-        if filename.endswith('.tar.gz'):
+        if filename.endswith(".tar.gz"):
             file_path = os.path.join(directory, filename)
             with tarfile.open(file_path, "r:gz") as tar:
-                extract_dir = os.path.join(save_path, filename.replace('.tar.gz', ''))
+                extract_dir = os.path.join(save_path, filename.replace(".tar.gz", ""))
                 create_dir(extract_dir)
                 tar.extractall(path=extract_dir)
                 print(f"Extracted {filename} to {extract_dir}")
 
+
 def main():
-    data_paths = ["../../data/SciGen/test-CL/test_CL_all_meta_all_formats_2024_11_25.hf", 
-                 "../../data/SciGen/test-Other/test_Other_all_meta_all_formats_2024_11_25.hf"]
-    
-    dowload_paths = ["../../data/SciGen/test-Other/latex_source_code/", 
-                     "../../data/SciGen/test-Other/latex_source_code/"]
+    data_paths = [
+        "../../data/SciGen/test-CL/test_CL_all_meta_all_formats_2024_11_25.hf",
+        "../../data/SciGen/test-Other/test_Other_all_meta_all_formats_2024_11_25.hf",
+    ]
+
+    dowload_paths = [
+        "../../data/SciGen/test-Other/latex_source_code/",
+        "../../data/SciGen/test-Other/latex_source_code/",
+    ]
 
     for data_path, download_path in zip(data_paths, dowload_paths):
         scigen_data = load_from_disk(data_path)
         scigen_data = scigen_data.to_pandas()
-        arxiv_ids = list(set(scigen_data.loc[scigen_data["venue"] == "arxiv", "paper_id"]))
+        arxiv_ids = list(
+            set(scigen_data.loc[scigen_data["venue"] == "arxiv", "paper_id"])
+        )
 
         for paper_id in arxiv_ids:
             download_arxiv_source(paper_id, download_path)
 
-    unzip_save_paths = ["../../../data/SciGen/test-CL/latex_source_code/unzipped/", 
-                        "../../../data/SciGen/test-Other/latex_source_code/unzipped/"]
+    unzip_save_paths = [
+        "../../../data/SciGen/test-CL/latex_source_code/unzipped/",
+        "../../../data/SciGen/test-Other/latex_source_code/unzipped/",
+    ]
 
     for download_path, save_path in zip(dowload_paths, unzip_save_paths):
         try:
@@ -76,5 +93,6 @@ def main():
         except Exception as e:
             print(f"Error extracting tar: {e} for dir: {download_path}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

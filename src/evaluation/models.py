46e32d0170051659abdd4cafabc702dd65c9c6a9
barthfab <fabio.barth@gmx.de>
2025-01-16 21:12:43 +0100

    generating logits with text

--- src/evaluation/models.py
+++ src/evaluation/models.py
@@ -2,7 +2,7 @@ from abc import abstractmethod
 from abc import ABC, abstractmethod
 from typing import Dict, List
 from transformers import pipeline, set_seed
-import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 class LanguageModel(ABC):
@@ -15,16 +15,14 @@ class LanguageModel(ABC):
         pass
 
 
-class HFModel(LanguageModel):
+class HFPipeModel(LanguageModel):
     def __init__(
         self,
         model_name: str,
         model_args: Dict,
         batch_size: int = 1,
-        device: str = "cuda",
+        device: str = "cuda:0",
         random_seed: int = 0,
-        numpy_random_seed: int = 1234,
-        torch_random_seed: int = 1234,
     ) -> None:
         super().__init__()
 
@@ -40,6 +38,7 @@ class HFModel(LanguageModel):
                 device=self.device,
                 trust_remote_code=True,
                 model_kwargs=self.model_args,
+                output_scores=True,
             )
         else:
             self.pipe = pipeline(
@@ -47,10 +46,55 @@ class HFModel(LanguageModel):
                 model=model_name,
                 device=self.device,
                 trust_remote_code=True,
+                output_scores=True,
             )
 
     def __call__(self, input: List, **kwargs):
-        return self.pipe(input, batch_size=self.batch_size, **kwargs)[0]
+        return self.pipe(
+            input, batch_size=self.batch_size, output_scores=True, **kwargs
+        )
+
+    def get_model_info(self):
+        return f"HF Model: {self.model_name}"
+
+
+class HFModel(LanguageModel):
+    def __init__(
+        self,
+        model_name: str,
+        model_args: Dict,
+        batch_size: int = 1,
+        device: str = "cuda:0",
+        random_seed: int = 0,
+    ) -> None:
+        super().__init__()
+
+        set_seed(random_seed)
+        self.model_name = model_name
+        self.model_args = model_args
+        self.device = device
+        self.batch_size = batch_size
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name, device_map=device, **self.model_args
+        )
+
+    def __call__(self, prompts: List, **kwargs):
+        inputs = self.tokenizer(prompts, return_tensors="pt")
+        output = self.model.generate(
+            **inputs,
+            num_return_sequences=1,
+            return_dict_in_generate=True,
+            output_scores=True,
+            **kwargs,
+        )
+        generated_text = [
+            self.tokenizer.decode(seq, skip_special_tokens=True)
+            for seq in output.sequences
+        ]  # skip_special_token = true
+        # logits shape: (batch_size, vocab_size)
+        logits = output.scores
+        return generated_text, logits
 
     def get_model_info(self):
         return f"HF Model: {self.model_name}"
